59233034510af7a2ed234d88a26126e64342a1bb
Cláudio Vinícius <claudiodev175@gmail.com>
2025-01-14 21:17:05 -0300

    Criação do cabeçalho e do rodapé

---
+++ src/app/components/cabecalho/cabecalho.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-cabecalho',
+  imports: [FontAwesomeModule],
+  templateUrl: './cabecalho.component.html',
+  styleUrl: './cabecalho.component.css'
+})
+export class CabecalhoComponent {
+  faPlus = faPlus;
+}
