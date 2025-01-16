3604dba837ed08f776f344e92f5752e7872aa68a
Cláudio Vinícius <claudiodev175@gmail.com>
2025-01-16 19:36:11 -0300

    Criação de 2 novos components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
 import { RodapeComponent } from './components/rodape/rodape.component';
+import { CartaoComponent } from './components/cartao/cartao.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CabecalhoComponent,RodapeComponent],
+  imports: [RouterOutlet, CabecalhoComponent,RodapeComponent, CartaoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/cartao/cartao.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cartao',
+  imports: [],
+  templateUrl: './cartao.component.html',
+  styleUrl: './cartao.component.css'
+})
+export class CartaoComponent {
+
+}
